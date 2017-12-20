76852207cd6b19e78f39994934c3d22f950164e8
vigzmv <vigzmv@outlook.com>
2017-12-20 18:40:40 +0530

    Removed HMR code

--- 15-Fetching/src/index.js
+++ 15-Fetching/src/index.js
@@ -3,8 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// Hot Module Replacement
-if (module.hot) {
-  module.hot.accept();
-}
--- xx-Toggler-with-Text/src/index.js
+++ xx-Toggler-with-Text/src/index.js
@@ -8,8 +8,3 @@ function App() {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// Hot Module Replacement
-if (module.hot) {
-  module.hot.accept();
-}
